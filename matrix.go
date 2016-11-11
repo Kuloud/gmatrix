1c29cfd429d82fbca840808328ede1e3819103cb
Kuloud <kuloud@kuloud.com>
2016-11-11 15:35:15 +0800

    init the project

---
+++ matrix.go
@@ -0,0 +1,142 @@
+package gmatrix
+
+import (
+	"bytes"
+	"strconv"
+)
+
+type Matrix struct {
+	rows, columns int       // the number of rows and columns.
+	data          []float32 // the contents of the matrix as one long slice.
+}
+
+// Set lets you define the value of a matrix at the given row and
+// column.
+
+func (A *Matrix) Set(r int, c int, val float32) {
+	A.data[findIndex(r, c, A)] = val
+}
+
+// Get retrieves the contents of the matrix at the row and column.
+
+func (A *Matrix) Get(r, c int) float32 {
+	return A.data[findIndex(r, c, A)]
+}
+
+// Column returns a slice that represents a column from the matrix.
+// This works by examining each row, and adding the nth element of
+// each to the column slice.
+
+func (A *Matrix) Column(n int) []float32 {
+	col := make([]float32, A.rows)
+	for i := 0; i < A.rows; i++ {
+		col[i] = A.Row(i)[n]
+	}
+	return col
+}
+
+// Row returns a slice that represents a row from the matrix.
+
+func (A *Matrix) Row(n int) []float32 {
+	return A.data[findIndex(n, 0, A):findIndex(n, A.columns, A)]
+}
+
+// Multiply multiplies two matrices together and return the resulting matrix.
+// For each element of the result matrix, we get the dot product of the
+// corresponding row from matrix A and column from matrix B.
+
+func Multiply(A, B *Matrix) *Matrix {
+	C := Zeros(A.rows, B.columns)
+	for r := 0; r < C.rows; r++ {
+		A_row := A.Row(r)
+		for c := 0; c < C.columns; c++ {
+			B_col := B.Column(c)
+			C.Set(r, c, dotProduct(A_row, B_col))
+		}
+	}
+	return C
+}
+
+// Add adds two matrices together and returns the resulting matrix.  To do
+// this, we just add together the corresponding elements from each matrix.
+
+func Add(A, B *Matrix) *Matrix {
+	C := Zeros(A.rows, A.columns)
+	for r := 0; r < A.rows; r++ {
+		for c := 0; c < A.columns; c++ {
+			C.Set(r, c, A.Get(r, c) + B.Get(r, c))
+		}
+	}
+	return C
+}
+
+// Identity creates an identity matrix with n rows and n columns.  When you
+// multiply any matrix by its corresponding identity matrix, you get the
+// original matrix.  The identity matrix looks like a zero-filled matrix with
+// a diagonal line of one's starting at the upper left.
+
+func Identity(n int) *Matrix {
+	A := Zeros(n, n)
+	for i := 0; i < len(A.data); i += (n + 1) {
+		A.data[i] = 1
+	}
+	return A
+}
+
+// Zeros creates an r x c sized matrix that's filled with zeros.  The initial
+// state of an int is 0, so we don't have to do any initialization.
+
+func Zeros(r, c int) *Matrix {
+	return &Matrix{r, c, make([]float32, r * c)}
+}
+
+// New creates an r x c sized matrix that is filled with the provided data.
+// The matrix data is represented as one long slice.
+
+func New(r, c int, data []float32) *Matrix {
+	if len(data) != r * c {
+		panic("[]int data provided to matrix.New is great than the provided capacity of the matrix!'")
+	}
+	A := Zeros(r, c)
+	A.data = data
+	return A
+}
+
+// findIndex takes a row and column and returns the corresponding index
+// from the underlying data slice.
+
+func findIndex(r, c int, A *Matrix) int {
+	return r * A.columns + c
+}
+
+// dotProduct calculates the algebraic dot product of two slices.  This is just
+// the sum  of the products of corresponding elements in the slices.  We use
+// this when we multiply matrices together.
+
+func dotProduct(a, b []float32) float32 {
+	var total float32
+	for i := 0; i < len(a); i++ {
+		total += a[i] * b[i]
+	}
+	return total
+}
+
+func (m *Matrix)String() string {
+	var bf bytes.Buffer
+	bf.WriteByte('[')
+	for r := 0; r < m.rows; r++ {
+		for c := 0; c < m.columns; c++ {
+			bf.WriteString(strconv.FormatFloat(float64(m.data[findIndex(r, c, m)]), 'f', -1, 32))
+			if c == m.columns - 1 {
+				if r == m.rows - 1 {
+					bf.WriteByte(']')
+				} else {
+					bf.WriteString(",\n ")
+				}
+			} else {
+				bf.WriteString(", ")
+			}
+		}
+	}
+	return bf.String()
+}
---
+++ matrix_test.go
@@ -0,0 +1,20 @@
+package gmatrix
+
+import (
+	"testing"
+	"fmt"
+)
+
+func TestNew(t *testing.T) {
+	m := &Matrix{
+		rows:2,
+		columns:3,
+		data:[]float32{1, 2, 3, 4, 5, 6},
+	}
+	n := Zeros(3, 2)
+	fmt.Println(m.String())
+	m.Set(1, 2, 3.3)
+	fmt.Println(m.String())
+	fmt.Println(m.Get(1, 2))
+	fmt.Println(Multiply(m, n))
+}
